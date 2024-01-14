e235f512e0947d18ad892a86ead5c30e6b52a564
Naveen Kolambage <naveenkolambage@gmail.com>
2024-01-15 08:14:50 +1300

    added main screens with tailwindcss enabled

---
+++ src/navigation/index.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screen/HomeScreen";
+import WelcomeScreen from "../screen/WelcomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome "
+        screenOptions={{ headershown: false }}
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
