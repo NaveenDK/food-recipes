56d6ed8910695df86abade87ff5b4334d3a2c7f2
Naveen Kolambage <naveenkolambage@gmail.com>
2024-01-20 11:55:09 +1300

    added HomeScreen structure

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,9 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
+
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function HomeScreen() {
   return (
-    <View>
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        <View className="mx-5 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/images/avatar.png")}
+            style={{ height: hp(5), width: hp(5.5) }}
+          />
+        </View>
+      </ScrollView>
       <Text>HomeScreen</Text>
     </View>
   );
