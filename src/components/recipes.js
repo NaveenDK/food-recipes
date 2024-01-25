f661a89122fd47bd9e3117199dc6e7f5a721c450
Naveen Kolambage <naveenkolambage@gmail.com>
2024-01-25 23:09:09 +1300

    added Recipes masonry layout

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, Image } from "react-native";
 import React from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import MasonryList from "@react-native-seoul/masonry-list";
+import { mealData } from "../constants";
 
 export default function Recipes() {
   return (
@@ -14,6 +16,35 @@ export default function Recipes() {
       >
         Recipes
       </Text>
+      <View>
+        <MasonryList
+          data={mealData}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item, i }) => <RecipeCard item={item} index={i} />}
+          //    onRefresh={() => refetch({ first: ITEM_CNT })}
+          onEndReachedThreshold={0.1}
+          // onEndReached={() => loadNext(ITEM_CNT)}
+        />
+      </View>
     </View>
   );
 }
+
+const RecipeCard = ({ item, index }) => {
+  return (
+    <View>
+      <Pressable
+        style={{ width: "100%" }}
+        className="flex justify-center mb-4 space-y-1"
+      >
+        <Image
+          source={{ uri: item.image }}
+          style={{ width: "100%", height: hp(35), borderRadius: 35 }}
+          className="bg-black/5"
+        />
+      </Pressable>
+    </View>
+  );
+};
