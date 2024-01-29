8b9b1f2a88b3214c403921e6d12b03104908d9c6
Naveen Kolambage <naveenkolambage@gmail.com>
2024-01-29 13:41:32 +1300

    added favourite, back icons

--- src/screen/RecipeDetails.js
+++ src/screen/RecipeDetails.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { View, Text, Pressable, Image, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { CachedImage } from "../helpers/image";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
 
 const RecipeDetails = (props) => {
   let item = props.route.params;
+  const [isFavourite, setIsFavourite] = useState(false);
 
   return (
     <ScrollView
@@ -29,6 +39,30 @@ const RecipeDetails = (props) => {
             marginTop: 4,
           }}
         />
+      </View>
+      {/* header buttons*/}
+
+      <View className="w-full absolute flex-row justify-between items-center pt-14">
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+          className="p-2 rounded-full ml-5 bg-white"
+        >
+          <ChevronLeftIcon size={hp(3.5)} strokeWidth={4.5} color="#fbbf2f" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setIsFavourite(!isFavourite);
+          }}
+          className="p-2 rounded-full ml-5 bg-white mr-5"
+        >
+          <HeartIcon
+            size={hp(3.5)}
+            strokeWidth={4.5}
+            color={isFavourite ? "red" : "gray"}
+          />
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
