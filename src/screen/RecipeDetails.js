87f1830483d82a2f055b185213e68289e8179628
Naveen Kolambage <naveenkolambage@gmail.com>
2024-02-06 14:12:00 +1300

    added RecipeDetails

--- src/screen/RecipeDetails.js
+++ src/screen/RecipeDetails.js
@@ -14,13 +14,22 @@ import {
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import axios from "axios";
-import { ChevronLeftIcon } from "react-native-heroicons/outline";
-import { HeartIcon } from "react-native-heroicons/solid";
+import {
+  ChevronLeftIcon,
+  ClockIcon,
+  Square3Stack3DIcon,
+} from "react-native-heroicons/outline";
+import { HeartIcon, UsersIcon, FireIcon } from "react-native-heroicons/solid";
+import Loading from "../components/loading";
+import { useNavigation } from "@react-navigation/native";
 
 const RecipeDetails = (props) => {
   let item = props.route.params;
   const [isFavourite, setIsFavourite] = useState(false);
   const [meal, setMeal] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
+
   useEffect(() => {
     getMealData(item.idMeal);
   }, []);
@@ -35,6 +44,7 @@ const RecipeDetails = (props) => {
         // setMeal(response.data.strMeal);
         //console.log(response.data.meals[0]);
         setMeal(response.data.meals[0]);
+        setLoading(false);
       }
     } catch (err) {
       console.log(`error: `, err.message);
@@ -85,9 +95,132 @@ const RecipeDetails = (props) => {
           />
         </TouchableOpacity>
       </View>
-      <View>
-        <Text></Text>
-      </View>
+      {loading ? (
+        <Loading size="large" className="mt-16" />
+      ) : (
+        <View className="px-4 flex justify-between space-y-4 pt-8">
+          <View className="space-y-2">
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="font-bold flex-1 text-neutral-700"
+            >
+              {meal?.strMeal}
+            </Text>
+            <Text className="font-bold flex-1 text-neutral-500">
+              {meal?.strArea}
+            </Text>
+          </View>
+          {/* misc */}
+
+          <View className="flex-row justify-around">
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <ClockIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
+              </View>
+
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  35
+                </Text>
+                <Text
+                  style={{ fontSize: hp(1.3) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Mins
+                </Text>
+              </View>
+            </View>
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <UsersIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
+              </View>
+
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  3
+                </Text>
+                <Text
+                  style={{ fontSize: hp(1.3) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Servings
+                </Text>
+              </View>
+            </View>
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <FireIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
+              </View>
+
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  103
+                </Text>
+                <Text
+                  style={{ fontSize: hp(1.3) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Cal
+                </Text>
+              </View>
+            </View>
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <Square3Stack3DIcon
+                  size={hp(4)}
+                  strokeWidth={2.5}
+                  color="#525252"
+                />
+              </View>
+
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                ></Text>
+                <Text
+                  style={{ fontSize: hp(1.3) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Easy
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          {/* ingredients */}
+
+          <View className="space-y-4">
+            <Text
+              style={{ fontSize: hp(1.5) }}
+              className="font-bold flex-1 text-neutral-700"
+            >
+              Ingredients
+            </Text>
+          </View>
+        </View>
+      )}
     </ScrollView>
   );
 };
